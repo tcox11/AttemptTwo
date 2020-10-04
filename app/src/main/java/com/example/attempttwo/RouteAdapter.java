032f3941ae531421b96e285d3c74169ce65f0944
Tom <tcox1195@gmail.com>
2020-10-04 21:43:57 +0100

    border colour changes based on grade nexts step is added hold icons for different coloured holds

--- app/src/main/java/com/example/attempttwo/RouteAdapter.java
+++ app/src/main/java/com/example/attempttwo/RouteAdapter.java
@@ -1,16 +1,21 @@
 package com.example.attempttwo;
 
+import android.app.Activity;
+import android.graphics.Color;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 
@@ -27,9 +32,19 @@ public class RouteAdapter extends RecyclerView.Adapter<RouteAdapter.RouteHolder>
 
     @Override
     public void onBindViewHolder(@NonNull RouteHolder holder, int position) {
-        Log.d("hello", String.valueOf(position));
         Route currentRoute = routes.get(position);
-        String currentRouteName = currentRoute.getHoldColour() + " " + currentRoute.getGrade();
+        String currentRouteName = currentRoute.getArea() + ", "
+                                  + currentRoute.getHoldColour().toLowerCase() + " holds, "
+                                  + currentRoute.getGrade();
+        String colorName = currentRoute.getGrade().substring(0,2).toLowerCase() + "color";
+        int colorID = MyUtilites.getResId(colorName, R.color.class);
+        if (colorID == -1){
+            Log.d("hello", "missing colour");
+            holder.frameLayout.setBackgroundColor(Color.parseColor("#FFFFFF"));
+        } else {
+            holder.frameLayout.setBackgroundResource(colorID);
+            //holder.holdIcon.setImageResource(R.drawable.completed_button_clicked);
+        }
         holder.textViewRouteName.setText(currentRouteName);
         holder.checkBoxWatched.setChecked(currentRoute.getWatchlist()==1);
         holder.checkBoxCompleted.setChecked(currentRoute.getCompleted()==1);
@@ -50,12 +65,16 @@ public class RouteAdapter extends RecyclerView.Adapter<RouteAdapter.RouteHolder>
         private TextView textViewRouteName;
         private CheckBox checkBoxWatched;
         private CheckBox checkBoxCompleted;
+        private FrameLayout frameLayout;
+        private ImageView holdIcon;
 
         public RouteHolder(View itemView){
             super(itemView);
             textViewRouteName = itemView.findViewById(R.id.text_view_route_name);
             checkBoxWatched = itemView.findViewById(R.id.watchedCheckBox);
             checkBoxCompleted = itemView.findViewById(R.id.completedCheckBox);
+            frameLayout = itemView.findViewById(R.id.card_frame);
+            holdIcon = itemView.findViewById(R.id.hold_icon);
         }
     }
 }
