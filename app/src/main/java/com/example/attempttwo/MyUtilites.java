c310740d4984f1d2b891fc14a0c8ee639c5f8556
Tom <tcox1195@gmail.com>
2020-10-05 12:18:02 +0100

    added colored hold icons

---
+++ app/src/main/java/com/example/attempttwo/MyUtilites.java
@@ -0,0 +1,15 @@
+package com.example.attempttwo;
+
+import java.lang.reflect.Field;
+
+public class MyUtilites {
+    public static int getResId(String resName, Class<?> c) {
+        try {
+            Field idField = c.getDeclaredField(resName);
+            return idField.getInt(idField);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -1;
+        }
+    }
+}
